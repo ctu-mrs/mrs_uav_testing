2e74c92dd3f39241a440a8b127bc651aaf075384
Tomas Baca <klaxalk@gmail.com>
2020-05-20 11:46:45 +0200

    udated monitor.sh

--- scripts/monitor.sh
+++ scripts/monitor.sh
@@ -22,12 +22,13 @@ pre_input="export UAV_NAME=$UAV_NAME"
 input=(
   'Rviz' "waitForRos; unset ROS_IP; roscd mrs_uav_testing; ${SCRIPT_PATH}/change_uav_control.sh $UAV_NAME; rosrun rviz rviz -d ${SCRIPT_PATH}/../rviz/remote_log.rviz
   "
-  'RvizInterface' "waitForRos; unset ROS_IP; roslaunch mrs_rviz_interface mrs_rviz_interface.launch
+  'RvizInterface' "waitForRos; unset ROS_IP; roslaunch mrs_rviz_plugins rviz_interface.launch
   "
   'Juggler' "waitForRos; unset ROS_IP; sleep 2;  ${SCRIPT_PATH}/change_uav.sh $UAV_NAME; i3 workspace "9"; rosrun plotjuggler PlotJuggler -l ${SCRIPT_PATH}/../plot_juggler/odometry.xml"
   'Reconfigure' " waitForRos; unset ROS_IP; rosrun rqt_reconfigure rqt_reconfigure"
   # 'Rosbag' "waitForRos; rosrun mrs_uav_testing record_remote.sh"
-  'Layout' "waitForRos; sleep 2; ~/.i3/layout_manager.sh ${SCRIPT_PATH}/../layouts/remote_log.json"
+  'Layout' "waitForRos; sleep 2; ~/.i3/layout_manager.sh ${SCRIPT_PATH}/../layouts/remote_log.json
+"
 )
 
 init_window="Rosout"
