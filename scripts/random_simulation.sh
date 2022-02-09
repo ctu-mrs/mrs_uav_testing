1e8749a44cc0a317e33f676c1d0ade782e190e76
Vaclav Pritzl <pritzvac@fel.cvut.cz>
2022-02-09 21:48:39 +0100

    fix for randomized uav names in singularity

--- scripts/random_simulation.sh
+++ scripts/random_simulation.sh
@@ -3,14 +3,6 @@
 # get path to script
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-if [ -x "$(whereis nvim | awk '{print $2}')" ]; then
- VIM_BIN="$(whereis nvim | awk '{print $2}')"
- HEADLESS="--headless"
-elif [ -x "$(whereis vim | awk '{print $2}')" ]; then
- VIM_BIN="$(whereis vim | awk '{print $2}')"
- HEADLESS=""
-fi
-
 cp $SCRIPT_PATH/../rviz/default_random_simulation.rviz /tmp/default_random_simulation.rviz
 
-$VIM_BIN -u "$GIT_PATH/linux-setup/submodules/profile_manager/epigen/epigen.vimrc" $HEADLESS -Ens -c "%s/uav[0-9]\+/$UAV_NAME/g" -c "wqa" -- "/tmp/default_random_simulation.rviz"
+sed -i "s/uav[0-9]/$UAV_NAME/g" /tmp/default_random_simulation.rviz
