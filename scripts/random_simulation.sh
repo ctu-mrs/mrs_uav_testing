a5409b412aaf7bab922a752f1f99912dc2a3857a
Tomas Baca <klaxalk@gmail.com>
2022-02-20 17:15:59 +0100

    fixed randomizer script

--- scripts/random_simulation.sh
+++ scripts/random_simulation.sh
@@ -3,6 +3,8 @@
 # get path to script
 SCRIPT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-cp $SCRIPT_PATH/../rviz/default_random_simulation.rviz /tmp/default_random_simulation.rviz
+PACKAGE_PATH=$( rospack find mrs_uav_testing )
+
+cp $PACKAGE_PATH/rviz/default_random_simulation.rviz /tmp/default_random_simulation.rviz
 
 sed -i "s/uav[0-9]/$UAV_NAME/g" /tmp/default_random_simulation.rviz
