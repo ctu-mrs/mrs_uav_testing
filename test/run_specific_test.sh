d3b9adb070b9c71af61ad1394cc2acdf5067cdc3
Tomas Baca <Tomas Baca>
2025-04-29 10:07:57 +0200

    updated tetst scripts

--- test/run_specific_test.sh
+++ test/run_specific_test.sh
@@ -11,6 +11,6 @@ done
 
 colcon test-result --delete-yes
 
-colcon test --packages-select mrs_uav_testing --ctest-args -R 'test_takeoff'
+colcon test --packages-select mrs_uav_testing --ctest-args -R 'takeoff'
 
 colcon test-result --all --verbose
